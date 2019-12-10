e4071212c640b9ffec7f317e61cf46c896f2cfa4
dcorre <corre@lal.in2p3.fr>
2019-12-10 11:29:36 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='colibri_obs_strategy',
+   version='1.0',
+   author='David Corre',
+   author_email='david.corre.fr@gmail.com',
+   packages=['colibri_obs_strategy'],
+   license='LICENSE.txt',
+   zip_safe=False,
+   install_requires=['numpy','scipy','astropy']
+)
